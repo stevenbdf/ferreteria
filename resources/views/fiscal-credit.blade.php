9cd66c8ee853365a00ff6a1683548ecdb14b135c
Steven Diaz <stevenbdf@gmail.com>
2021-02-09 23:23:21 -0600

    🐛  Fix description length and font size quote

--- resources/views/fiscal-credit.blade.php
+++ resources/views/fiscal-credit.blade.php
@@ -65,229 +65,30 @@
         <tr>
             <td>
                 <table style="width:100%;padding-top:1.35cm;height:10.3cm;">
+                @for ($i = 0; $i < 10; $i++)
                     <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;padding-top:0.5cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
+                        <td style="vertical-align:top;padding-bottom:0cm;padding-top:0.2cm;">
+                            <table style="width:100%;padding-left:1cm;height:0.7cm;">
                                 <tr>
                                     <td style="width:1.3cm;">
-                                        {{ array_key_exists(0, $fiscal_credit_details) ? $fiscal_credit_details[0]['quantity'] : ' ' }}
+                                        {{ isset($fiscal_credit_details[$i]['quantity']) ? $fiscal_credit_details[$i]['quantity'] : ' ' }}
                                     </td>
-                                    <td>
-                                        {{ array_key_exists(0, $fiscal_credit_details) ? $fiscal_credit_details[0]['product']['description'] : ' ' }}
+                                    <td style="font-size:14px;">
+                                        {{ isset($fiscal_credit_details[$i]['product']['description']) ? substr($fiscal_credit_details[$i]['product']['description'], 0, 30) : ' ' }}
                                     </td>
                                     <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(0, $fiscal_credit_details) ? number_format($fiscal_credit_details[0]['sale_price'], 2) : ' ' }}
+                                        {{ isset($fiscal_credit_details[$i]['sale_price']) ? number_format($fiscal_credit_details[$i]['sale_price'], 2) : ' ' }}
                                     </td>
                                     <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(0, $fiscal_credit_details) ? number_format($fiscal_credit_details[0]['sub_total'], 2): ' ' }}
+                                        {{ isset($fiscal_credit_details[$i]['sub_total']) ? number_format($fiscal_credit_details[$i]['sub_total'], 2) : ' ' }}
                                     </td>
                                 </tr>
                             </table>
                         </td>
                     </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(1, $fiscal_credit_details) ? $fiscal_credit_details[1]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(1, $fiscal_credit_details) ? $fiscal_credit_details[1]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(1, $fiscal_credit_details) ? number_format($fiscal_credit_details[1]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(1, $fiscal_credit_details) ? number_format($fiscal_credit_details[1]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(2, $fiscal_credit_details) ? $fiscal_credit_details[2]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(2, $fiscal_credit_details) ? $fiscal_credit_details[2]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(2, $fiscal_credit_details) ? number_format($fiscal_credit_details[2]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(2, $fiscal_credit_details) ? number_format($fiscal_credit_details[2]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(3, $fiscal_credit_details) ? $fiscal_credit_details[3]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(3, $fiscal_credit_details) ? $fiscal_credit_details[3]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(3, $fiscal_credit_details) ? number_format($fiscal_credit_details[3]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(3, $fiscal_credit_details) ? number_format($fiscal_credit_details[3]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(4, $fiscal_credit_details) ? $fiscal_credit_details[4]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(4, $fiscal_credit_details) ? $fiscal_credit_details[4]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(4, $fiscal_credit_details) ? number_format($fiscal_credit_details[4]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(4, $fiscal_credit_details) ? number_format($fiscal_credit_details[4]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(5, $fiscal_credit_details) ? $fiscal_credit_details[5]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(5, $fiscal_credit_details) ? $fiscal_credit_details[5]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(5, $fiscal_credit_details) ? number_format($fiscal_credit_details[5]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(5, $fiscal_credit_details) ? number_format($fiscal_credit_details[5]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(6, $fiscal_credit_details) ? $fiscal_credit_details[6]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(6, $fiscal_credit_details) ? $fiscal_credit_details[6]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(6, $fiscal_credit_details) ? number_format($fiscal_credit_details[6]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(6, $fiscal_credit_details) ? number_format($fiscal_credit_details[6]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(7, $fiscal_credit_details) ? $fiscal_credit_details[7]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(7, $fiscal_credit_details) ? $fiscal_credit_details[7]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(7, $fiscal_credit_details) ? number_format($fiscal_credit_details[7]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(7, $fiscal_credit_details) ? number_format($fiscal_credit_details[7]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(8, $fiscal_credit_details) ? $fiscal_credit_details[8]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(8, $fiscal_credit_details) ? $fiscal_credit_details[8]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(8, $fiscal_credit_details) ? number_format($fiscal_credit_details[8]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(8, $fiscal_credit_details) ? number_format($fiscal_credit_details[8]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(9, $fiscal_credit_details) ? $fiscal_credit_details[9]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(9, $fiscal_credit_details) ? $fiscal_credit_details[9]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(9, $fiscal_credit_details) ? number_format($fiscal_credit_details[9]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(9, $fiscal_credit_details) ? number_format($fiscal_credit_details[9]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align:top;padding-bottom:0cm;">
-                            <table style="width:100%;padding-left:1cm;height:0.75cm;">
-                                <tr>
-                                    <td style="width:1.3cm;">
-                                        {{ array_key_exists(10, $fiscal_credit_details) ? $fiscal_credit_details[10]['quantity'] : ' ' }}
-                                    </td>
-                                    <td>
-                                        {{ array_key_exists(10, $fiscal_credit_details) ? $fiscal_credit_details[10]['product']['description'] : ' ' }}
-                                    </td>
-                                    <td style="width:0.8cm;padding-right:2cm;">
-                                        {{ array_key_exists(10, $fiscal_credit_details) ? number_format($fiscal_credit_details[10]['sale_price'], 2) : ' ' }}
-                                    </td>
-                                    <td style="text-align: right;padding-right:1.2cm;width:1.7cm;">
-                                        {{ array_key_exists(10, $fiscal_credit_details) ? number_format($fiscal_credit_details[10]['sub_total'], 2): ' ' }}
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
+                    @endfor
                     <tr>
                         <td>
-
                             <table style="padding-left:1cm;">
                                 <tr>
                                     <td style="font-size: 11px;width:7cm;padding-left:0.5cm;vertical-align:top;padding-top:0.3cm">
